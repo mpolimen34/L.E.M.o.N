f0b12f694c4bd5128dc110fa8c1322c741350a66
Marco Polimeni <stats3434@gmail.com>
2020-06-23 12:59:49 -0400

    Updated Table Values

--- code/database/db_handler.py
+++ code/database/db_handler.py
@@ -1,20 +1,33 @@
 import sqlite3
 
-conn = sqlite3.connect('test-base.db')
+conn = sqlite3.connect('proto-base.db')
 
 c = conn.cursor()
 
 # c.execute("""CREATE TABLE test (
-#           name text,
-#           heat integer
-#           )""")
+#           id INTEGER PRIMARY KEY,
+#           name TEXT,
+#           heat INTEGER,
+#           date DATE,
+#           time TIME,
+#           weight FLOAT
+#           );""")
 
-def insert_new_food(name, temp):
+def insert_new_food(name, heat, date, time, weight):
   with conn: # Commits the change to the database
-    c.execute("INSERT INTO test VALUES (:name, :heat)", {'name': name, 'heat': temp})
+    c.execute("""INSERT INTO test (name, heat, date, 
+              time, weight) VALUES (?, ?, ?, ?, ?);""",
+              (name, heat, date, time, weight))
 
 def get_food_info(name):
   c.execute("SELECT * FROM test WHERE name=:name", {'name': name})
   return c.fetchall()
+
+def get_table():
+  c.execute("SELECT * FROM test;")
+  return c.fetchall()
+
+# insert_new_food("pizza", 65, '2018-06-23', '09:10:00', 12)
+# print(get_table())
 
 conn.close()
