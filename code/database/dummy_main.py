50f329910e9b2351d12933f737120731b28b7a0d
Marco Polimeni <stats3434@gmail.com>
2020-06-29 15:30:00 -0400

    Completed dummy data submission

--- code/database/dummy_main.py
+++ code/database/dummy_main.py
@@ -1,10 +1,25 @@
 import db_handler
-import datetime
-
-# print(db_handler.get_table())
+from datetime import datetime
+import random
+import time
 
 def main():
-  pass
+  print(db_handler.get_table())
+  for i in range(25):
+    now = datetime.now()
+    db_handler.insert_new_food(random_food(), random.randint(40, 100), 
+                                now.strftime("%Y-%m-%d"), 
+                                now.strftime("%H:%M:%S"), 
+                                round(random.uniform(.25, 5.5), 3))
+    time.sleep(2)
+  print(db_handler.get_table())
+
+def random_food():
+  foods = ["hot dog", "pizza", "burger", "french fries", "lettuce",
+           "corn", "pasta", "macaroni and cheese", "chicken",
+           "breaded chicken", "yogurt", "omelette", "sandwich",
+            "cereal", "drink", "pork chop", "fruit"]
+  return random.choice(foods)
 
 if __name__ == "__main__":
   main()
